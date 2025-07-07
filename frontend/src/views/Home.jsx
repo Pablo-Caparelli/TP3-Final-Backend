9ea2e74e26fb7d655dba8553714f8cb29e142b59
pablo-caparelli <pablocaparelli@gmail.com>
2025-07-07 12:40:28 -0300

    segundo commit con función handleSearch que tiene setFilteredProducts y setSearchInput usando método filter para filtrar productos y método toLowerCase para que traiga mayúsculas y minúsculas, también incorporación de clases para los estilos

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -5,7 +5,8 @@ import { useAuth } from "../context/AuthContext";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
   const { user } = useAuth();
 
   const fetchProducts = async () => {
@@ -14,6 +15,7 @@ const Home = () => {
 
     if (response.ok) {
       setProducts(responseToJson.data);
+      setFilteredProducts(responseToJson.data);
     }
   };
 
@@ -33,6 +35,14 @@ const Home = () => {
     fetchProducts();
   }, []);
 
+  const handleSearch = () => {
+    const filtered = products.filter((product) =>
+      product.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+    setSearchInput(""); // limpia el input después de filtrar
+  };
+
   return (
     <Layout>
       <h1>Bienvenido a nuestra tienda de productos artesanales</h1>
@@ -40,25 +50,48 @@ const Home = () => {
         Descubrí nuestra selección exclusiva de productos únicos hechos a mano.
         Calidad y diseño en cada detalle.
       </p>
+
+      <div className="search">
+        <label className="search-label" htmlFor="search">
+          Buscar por nombre:
+        </label>
+        <input
+          id="search"
+          type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          placeholder="Ingrese nombre del producto"
+        />
+        <button className="search-button" onClick={handleSearch}>
+          Buscar y limpiar input
+        </button>
+      </div>
+
       <section>
-        {products.map((product) => (
-          <div key={product._id}>
-            <p>
-              <b>Nombre:</b> {product.name}
-            </p>
-            <p>
-              <b>Precio:</b> {product.price}
-            </p>
-            <p>
-              <b>Categoria:</b> {product.category}
-            </p>
-            {user && (
-              <div className="cont-button-product">
-                <button onClick={() => handleClick(product._id)}>Borrar</button>
-              </div>
-            )}
-          </div>
-        ))}
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <div key={product._id}>
+              <p>
+                <b>Nombre:</b> {product.name}
+              </p>
+              <p>
+                <b>Precio:</b> {product.price}
+              </p>
+              <p>
+                <b>Categoria:</b> {product.category}
+              </p>
+              {user && (
+                <div className="cont-button-product">
+                  <button onClick={() => handleClick(product._id)}>
+                    Borrar
+                  </button>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <p>No se encontraron los productos buscados.</p>
+        )}
       </section>
     </Layout>
   );
